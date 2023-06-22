0701ef49da8b9f81cdc260adc6dd89683c55a2c3
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-06-22 15:22:36 +0200

    Fatto Header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,10 +4,45 @@ export default {
 }
 </script>
 <template>
-    <div>
-        
+    <div class="container d-flex align-start">
+        <div class="blue">
+            
+        </div>
+        <div class="red">
+            
+        </div>
+        <div class="yellow">
+    
+        </div>
+        <div class="green">
+    
+        </div>
     </div>
 </template>
 <style lang="scss" scoped>
-    
+    .blue, .red, .yellow, .green{
+        border: 3px solid #fff;
+        margin-right: 10px;
+        display: inline-block;
+        border-radius: 50%;
+    }
+    .blue{
+        width: 50px;
+        height: 50px;
+        background-color: blue;
+    }
+
+    .red, .yellow, .green{
+        width: 30px;
+        height: 30px;
+    }
+    .red{
+        background-color: red;
+    }
+    .yellow{
+        background-color: yellow;
+    }
+    .green{
+        background-color: green;
+    }
 </style>
